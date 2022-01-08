6bcea3f425f42e660c31a4a24ca340a9a593eb7d
sunilhp <sunil.hp@gmail.com>
2022-01-08 17:41:45 +0530

    mysql db

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksController } from './tasks.controller';
+import { TaskRepositary } from './task.repositary';
 import { TasksService } from './tasks.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TaskRepositary])],
   controllers: [TasksController],
   providers: [TasksService]
 })
