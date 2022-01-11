aeb92e923fbb3298ba8b81c8d983f74fb781eba3
sunilhp <sunil.hp@gmail.com>
2022-01-11 17:37:36 +0530

    passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,17 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepositary } from './user.repositary';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports : [
+    JwtModule.register({
+      secret: 'topsecret51',
+      signOptions : {
+        expiresIn : 3600,
+        
+      }
+    }),
     TypeOrmModule.forFeature([UserRepositary]),
   ],
   providers: [AuthService],
